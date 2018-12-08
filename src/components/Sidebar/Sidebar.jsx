2c2c35fab06625d52d1a904768acad18f5fc9efc
Pete Jeffryes <pjeffryes@tendrilinc.com>
2018-12-08 09:55:33 -0700

    extract sidebar config

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,85 +1,35 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
 import SidebarLink from  '../SidebarLink';
+import config from '../../config/sidebar';
 import './styles.scss';
+import { Logo } from '../Icons';
 
-import {
-  SessionsIcon,
-  DashboardIcon,
-  EventsIcon,
-  FilesBoardIcon,
-  ConversationsIcon,
-  SettingsIcon,
-  Logo
-} from '../Icons';
 
-class Sidebar extends React.PureComponent {
+const renderLinks = (props) => {
+  return config.map((link, i) => {
+    const SidebarIcon = link.icon;
+    return <SidebarLink
+      key={i}
+      to={link.to}
+      {...props}>
+      <SidebarIcon width={22}/>
+    </SidebarLink>;
+  });
+};
 
-  get links() {
-    return [
-      {
-        to: '/dashboard',
-        label: 'Dashboard',
-        icon: DashboardIcon,
-        exact: true,
-      },
-      {
-        to: '/sessions',
-        label: 'sessions',
-        icon: SessionsIcon,
-        exact: true,
-      },
-      {
-        to: '/',
-        label: 'events',
-        icon: EventsIcon,
-        exact: true,
-      },
-      {
-        to: '/files',
-        label: 'Files',
-        icon: FilesBoardIcon,
-        exact: true,
-      },
-      {
-        to: '/conversations',
-        label: 'Conversations',
-        icon: ConversationsIcon,
-        exact: true,
-      },
-      {
-        to: '/settings',
-        label: 'Settings',
-        icon: SettingsIcon,
-        exact: true,
-      },
-    ];
-  }
+const Sidebar = (props) => {
 
-  renderLinks() {
-    return this.links.map((link, i) => {
-      const Icon = link.icon;
-      return <SidebarLink
-        key={i}
-        {...link}
-        {...this.props}>
-        <Icon width={22}/>
-      </SidebarLink>;
-    });
-  }
-
-  render() {
-    return (
-      <div className='sidebar'>
-        <div className='sidebar--logo'>
-          <Logo width={'33%'} height={null}/>
-        </div>
-        <nav className='sidebar--nav'>
-          {this.renderLinks()}
-        </nav>
+  return (
+    <div className='sidebar'>
+      <div className='sidebar--logo'>
+        <Logo width={'33%'} height={null}/>
       </div>
-    );
-  }
-}
+      <nav className='sidebar--nav'>
+        {renderLinks(props)}
+      </nav>
+    </div>
+  );
+};
 
 export default withRouter(Sidebar);
